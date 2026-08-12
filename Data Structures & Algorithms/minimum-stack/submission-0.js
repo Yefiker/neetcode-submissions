e466ef36c19acf95cd37a9998ef31a292d4272a9
Yefiker <melkamken060@gmail.com>
2026-08-12 22:04:26 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,40 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        let top = this.minStack[this.minStack.length - 1];
+        let currentMin = this.minStack.length === 0 ? val : Math.min(val, top);
+        this.minStack.push(currentMin);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+        
+    }
+}
